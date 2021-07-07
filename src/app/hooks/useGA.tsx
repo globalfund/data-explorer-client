f0ce0625b6f24bd59c69f3ef8f3194757af78473
Stefanos <stefanos@zimmerman.team>
2021-07-07 18:34:08 +0300

    chore: add GA

---
+++ src/app/hooks/useGA.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+function sendPageView(location: any) {
+  // @ts-ignore
+  window.gtag("event", "page_view", {
+    page_title: document.title,
+    page_location: window.location.href,
+    page_path: location.pathname,
+    send_to: process.env.REACT_APP_GOOGLE_ANALYTICS_ID,
+  });
+}
+
+export function useGA() {
+  const history = useHistory();
+
+  React.useEffect(() => {
+    if (window.location.hostname === "the.data.explorer.nyuki.io") {
+      return history.listen(sendPageView);
+    }
+  }, [history]);
+
+  return null;
+}
