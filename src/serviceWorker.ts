921422dcc11da8f1ae2c29ba3a5e8a79614c17b6
Stefanos <stefanos@zimmerman.team>
2021-08-05 20:17:31 +0300

    chore: update swurl

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -39,7 +39,7 @@ export function register(config?: Config) {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.ts`;
+      const swUrl = `${process.env.PUBLIC_URL}/static/service-worker.ts`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
