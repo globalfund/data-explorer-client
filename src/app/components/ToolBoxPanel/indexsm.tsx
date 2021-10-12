9f9a2cb9341922b0f11598874fdbcdac2d04c790
Wei Guo <guow1227@gmail.com>
2021-10-12 12:09:40 +0200

    chore: fixed some error

--- src/app/components/ToolBoxPanel/indexsm.tsx
+++ src/app/components/ToolBoxPanel/indexsm.tsx
@@ -1,36 +1,51 @@
 import React from "react";
-import { IconButton } from "@material-ui/core";
+import { IconButton, Slide } from "@material-ui/core";
 import CloseOutlinedIcon from "@material-ui/icons/CloseOutlined";
 import { SubToolBoxPanel } from "./subToolBox";
 import { ToolBoxPanelProps } from ".";
 
 export function ToolBoxPage(props: ToolBoxPanelProps) {
   return (
-    <div
-      css={`
-        width: 100%;
-        height: 100%;
-        visibility: ${props.open ? "visible" : "hidden"};
-        opacity: ${props.open ? 1 : 0};
-        left: 0;
-        top: 40px;
-        position: fixed;
-        z-index: 15;
-      `}
-    >
-      <div>
-        <div css="height:24px;background-color: #373D43;width:100%;">
-          <IconButton
-            css="
-            width:12px;
-            height:12px;"
-            onClick={props.onButtonClick}
-          >
-            <CloseOutlinedIcon htmlColor="#ffffff" viewBox=" -7 -7 30 30" />
-          </IconButton>
+    <Slide direction="left" in={props.open}>
+      <div
+        css={`
+          right: 0;
+          top: 133px;
+          z-index: 20;
+          width: 500px;
+          position: fixed;
+          background: #f5f5f7;
+          height: calc(100vh - 133px);
+          visibility: visible !important;
+          box-shadow: 0px 0px 10px rgba(152, 161, 170, 0.6);
+
+          @media (max-width: 500px) {
+            width: calc(100vw - 50px);
+          }
+        `}
+      >
+        <div
+          css={`
+            width: 100%;
+            height: 100%;
+            display: flex;
+            position: relative;
+            flex-direction: column;
+          `}
+        >
+          <div>
+            <div css="height:24px;background-color: #373D43;width:100%;">
+              <IconButton
+                css="width:12px;height:12px;"
+                onClick={props.onButtonClick}
+              >
+                <CloseOutlinedIcon htmlColor="#ffffff" viewBox=" -4 -4 30 30" />
+              </IconButton>
+            </div>
+            <SubToolBoxPanel filterGroups={props.filterGroups} />
+          </div>
         </div>
-        <SubToolBoxPanel filterGroups={props.filterGroups} />
       </div>
-    </div>
+    </Slide>
   );
 }
--- src/app/modules/viz-module/index.tsx
+++ src/app/modules/viz-module/index.tsx
@@ -62,7 +62,6 @@ export default function VizModule() {
         align-items: center;
         flex-direction: column;
         justify-content: center;
-        visibility: ${isSmallScreen && openToolboxPage ? "hidden" : "visible"};
       `}
     >
       <PageHeader
@@ -201,20 +200,20 @@ export default function VizModule() {
           />
           <div
             css={`
-              left: 0;
-              top: 48px;
-              z-index: 15;
-              width: 100%;
-              height: 100%;
-              position: fixed;
-              background: rgba(35, 35, 35, 0.5);
-              opacity: ${openToolboxPanel && widthThreshold < 0 ? 1 : 0};
-              visibility: ${openToolboxPanel && widthThreshold < 0
-                ? "visible"
-                : "hidden"};
-              transition: visibility 225ms cubic-bezier(0, 0, 0.2, 1),
-                opacity 225ms cubic-bezier(0, 0, 0.2, 1);
-            `}
+          left: 0;
+          top: 48px;
+          z-index: 15;
+          width: 100%;
+          height: 100%;
+          position: fixed;
+          background: rgba(35, 35, 35, 0.5);
+          opacity: ${openToolboxPanel && widthThreshold < 0}? 1 : 0};
+          visibility: ${
+            openToolboxPanel && widthThreshold < 0 ? "visible" : "hidden"
+          };
+          transition: visibility 225ms cubic-bezier(0, 0, 0.2, 1),
+            opacity 225ms cubic-bezier(0, 0, 0.2, 1);
+        `}
           />
         </>
       )}
