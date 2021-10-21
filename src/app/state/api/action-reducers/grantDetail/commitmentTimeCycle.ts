83d300def96ba2b60ad9872976d952af11a35b06
Stefanos <stefanos@zimmerman.team>
2021-10-21 09:59:29 +0200

    chore: use grant commitment time cycle grant api

--- src/app/state/api/action-reducers/grantDetail/commitmentTimeCycle.ts
+++ src/app/state/api/action-reducers/grantDetail/commitmentTimeCycle.ts
@@ -2,7 +2,7 @@ import { APIModel } from "app/state/api";
 import { ApiCallModel } from "app/state/api/interfaces";
 
 const GrantDetailCommitmentTimeCycle: ApiCallModel = {
-  ...APIModel(`${process.env.REACT_APP_API}/commitment/time-cycle`),
+  ...APIModel(`${process.env.REACT_APP_API}/grant/commitment/time-cycle`),
 };
 
 export default GrantDetailCommitmentTimeCycle;
