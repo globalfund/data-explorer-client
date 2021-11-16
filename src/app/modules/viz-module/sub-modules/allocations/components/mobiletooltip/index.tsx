abc203d41f8410b23adf385100cce896dc9dd5f9
Stefanos <stefanos@zimmerman.team>
2021-11-16 18:47:13 +0200

    feat: implement mobile allocations radial chart

---
+++ src/app/modules/viz-module/sub-modules/allocations/components/mobiletooltip/index.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/core/IconButton";
+import { formatFinancialValue } from "app/utils/formatFinancialValue";
+import { AllocationsRadialMobileTooltipProps } from "app/modules/viz-module/sub-modules/allocations/data";
+
+export function AllocationsRadialMobileTooltip(
+  props: AllocationsRadialMobileTooltipProps
+) {
+  return (
+    <div
+      css={`
+        padding: 25px;
+        color: #262c34;
+        background: #fff;
+        border-radius: 20px;
+        box-shadow: 0px 0px 10px rgba(152, 161, 170, 0.3);
+      `}
+    >
+      <div
+        css={`
+            display: flex;
+            flex-direction: row;
+            justify-content flex-end;
+
+            path {
+              fill: #2E4063;
+            }
+          `}
+      >
+        <IconButton
+          onTouchStart={props.close}
+          css={`
+            padding: 0;
+          `}
+        >
+          <CloseIcon />
+        </IconButton>
+      </div>
+      <div
+        css={`
+          gap: 5px;
+          display: flex;
+          font-size: 12px;
+          flex-direction: row;
+          padding-bottom: 16px;
+          align-items: flex-end;
+          justify-content: space-between;
+          border-bottom: 1px solid #dfe3e6;
+        `}
+      >
+        <b>{props.label}</b>
+        <div css="white-space: nowrap;">
+          {formatFinancialValue(props.value)}
+        </div>
+      </div>
+      <Button
+        onTouchStart={props.drilldown}
+        css={`
+          width: 100%;
+          margin-top: 20px;
+          background: #dfe3e6;
+          border-radius: 22px;
+
+          &:hover {
+            background: #dfe3e6;
+          }
+
+          > span {
+            color: #262c34;
+            font-size: 14px;
+            font-weight: bold;
+            text-transform: none;
+            font-family: "GothamNarrow-Bold", "Helvetica Neue", sans-serif;
+          }
+        `}
+      >
+        Drill down
+      </Button>
+    </div>
+  );
+}
