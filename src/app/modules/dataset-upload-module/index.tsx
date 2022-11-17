ca9ee7daa333d3eff4928d1c4390451d87c1a408
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2022-11-17 12:43:14 +0100

    refactor: changed dataset creation and file upload to make use of axios

--- src/app/modules/dataset-upload-module/index.tsx
+++ src/app/modules/dataset-upload-module/index.tsx
@@ -9,47 +9,58 @@ import {
 import axios, { AxiosResponse, AxiosError } from 'axios';
 
 export default function DatasetUploadModule() {
-  const createDataset = useStoreActions(
-    (actions) => actions.dataThemes.DatasetCreate.post
-  );
-  const createdDatasetData = useStoreState(
-    (state) =>
-      (state.dataThemes.DatasetCreate.crudData ??
-        emptyDatasetAPI) as DatasetAPIModel
-  );
-  const createDatasetLoading = useStoreState(
-    (state) =>
-      state.dataThemes.DatasetCreate.loading
-  );
-  const createDatasetSuccess = useStoreState(
-    (state) => state.dataThemes.DatasetCreate.success
-  );
+  const [uploading, setUploading] = React.useState(false);
+  const [uploadSuccess, setUploadSuccess] = React.useState(false);
+
   const loadDatasets = useStoreActions(
     (actions) => actions.dataThemes.DatasetGetList.fetch
   );
 
   const onSubmit: any = async (event: any) => {
-    // Create a datatheme
-    createDataset({
-      values: {
-        "name": event.target.name.value,
-        "description": event.target.description.value,
-        "public": event.target.public.checked,
-        "category": event.target.datasetCategory.value
-      },
-    });
-  }
-
-  React.useEffect(() => {
-    if (createDatasetSuccess) {
-      // Reload the datasets for the data theme builder
-      loadDatasets({storeInCrudData: true});
-      // rename the uploaded file
-      const newName = 'dx' + createdDatasetData.id
-      console.log("LFM::The uploaded file's name should be: " + newName)
-      // move the uploaded file to the DX-backend/data folder.
+    event.preventDefault();
+    const datasetValues = {
+      "name": event.target.name.value,
+      "description": event.target.description.value,
+      "public": event.target.public.checked,
+      "category": event.target.datasetCategory.value
     }
-  }, [createDatasetSuccess, createDatasetLoading, createdDatasetData])
+    // Post the dataset
+    setUploading(true);
+    axios
+      .post(`${process.env.REACT_APP_API}/datasets`, datasetValues, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then(response => {
+        // if the dataset was created successfully, post the file to the server
+        const formData = new FormData();
+        let file = event.target.fileUpload.files[0];
+        let filename = 'dx'+response.data.id;
+        formData.append(filename, file);
+        axios
+          .post(`${process.env.REACT_APP_API}/files`, formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          })
+          .then(_ => {
+            setUploading(false);
+            setUploadSuccess(true);
+            loadDatasets({storeInCrudData: true});
+          })
+          .catch(error => {
+            console.debug("Dataset upload error", error)
+            setUploading(false);
+            setUploadSuccess(false);
+          })
+      })
+      .catch(error => {
+        console.debug("Dataset creation error", error)
+        setUploading(false);
+        setUploadSuccess(false);
+      })
+  }
 
   return (
     <div
@@ -64,15 +75,15 @@ export default function DatasetUploadModule() {
     >
       <PageTopSpacer />
       <h1>Dataset Upload</h1>
-      { createDatasetLoading &&
+      { uploading &&
         <PageLoader />
       }
-      { !createDatasetLoading && createdDatasetData.id === "" &&
+      { !uploading && !uploadSuccess &&
         <React.Fragment>
           <p>
             Please upload your JSON or CSV file. Your dataset will become available after being processed, which will roughly take around half a minute.
           </p>
-          <form action="/fileUpload" onSubmit={(event) => onSubmit(event)} method="post">
+          <form onSubmit={(event) => onSubmit(event)}>
             <label>Dataset name: </label><input name="name" type="text" placeholder="Type here..." required /><br />
             <label>Dataset description: </label><input name="description" type="text" placeholder="Type here..." required /><br />
             <label>Dataset category: </label><select name="datasetCategory">
@@ -80,12 +91,12 @@ export default function DatasetUploadModule() {
               <option value="other">Other</option>
             </select><br />
             <label>Publicly visible: </label><input name="public" type="checkbox" /><br />
-            <label>Dataset upload</label><input name="fileUpload" type="file" accept=".json,.csv"/><br />
+            <label>Dataset upload</label><input required name="fileUpload" type="file" accept=".json,.csv"/><br />
             <input type="submit" value="Upload your dataset" />
           </form>
         </React.Fragment>
       }
-      { createDatasetSuccess && createdDatasetData.id !== "" &&
+      { uploadSuccess &&
         <>
           <p>Your dataset has been created</p>
           <form action="/data-themes/new"><input type="submit" value="Create datatheme" ></input></form>
