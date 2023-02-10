e630e9010a37837595fc0c01afbf7b814c016c2b
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2023-02-10 16:52:11 +0200

    chore: cleanup

--- src/app/modules/grant-detail-module/index.tsx
+++ src/app/modules/grant-detail-module/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import get from "lodash/get";
 import { useMediaQuery } from "@material-ui/core";
 import { useTitle, useUpdateEffect } from "react-use";
-import { Switch, Route, useParams, useLocation } from "react-router-dom";
+import { Switch, Route, useParams } from "react-router-dom";
 import { useStoreActions, useStoreState } from "app/state/store/hooks";
 /* project */
 import { PageHeader } from "app/components/PageHeader";
@@ -23,21 +23,17 @@ import { GrantDetailInvestmentsTableWrapper } from "app/modules/viz-module/sub-m
 import { GrantDetailGenericBudgetsTimeCycleWrapper } from "app/modules/viz-module/sub-modules/budgets/time-cycle/data-wrappers/grantDetail";
 import { GrantDetailInvestmentsDisbursedWrapper } from "app/modules/viz-module/sub-modules/investments/disbursed/data-wrappers/grantDetail";
 import { GrantDetailInvestmentsTimeCycleWrapper } from "app/modules/viz-module/sub-modules/investments/time-cycle/data-wrappers/grantDetail";
-import BreadCrumbs from "app/components/Charts/common/breadcrumbs";
-import { useRecoilState } from "recoil";
-import { breadCrumbItems } from "app/state/recoil/atoms";
 
 export default function GrantDetail() {
   useTitle("The Data Explorer - Grant");
-  const location = useLocation();
   const vizWrapperRef = React.useRef(null);
   const datasetMenuItems = useDatasetMenuItems();
   const isMobile = useMediaQuery("(max-width: 767px)");
+  const isSmallScreen = useMediaQuery("(max-width: 960px)");
   const params = useParams<{ code: string; period: string; vizType: string }>();
   const [openToolboxPanel, setOpenToolboxPanel] = React.useState(
     !isMobile && params.vizType !== "overview"
   );
-  const [breadCrumbList, setBreadCrumList] = useRecoilState(breadCrumbItems);
 
   // api call & data
   const fetchGrantInfoData = useStoreActions(
@@ -111,17 +107,6 @@ export default function GrantDetail() {
     return 0;
   }
 
-  const isSmallScreen = useMediaQuery("(max-width: 960px)");
-
-  React.useEffect(() => {
-    if (!breadCrumbList.find((item) => item.name === grantInfoData.location))
-      setBreadCrumList([
-        ...breadCrumbList,
-        { name: grantInfoData.location, path: location.pathname, id: "" },
-      ]);
-  }, []);
-  console.log(grantInfoData.location, "location");
-
   return (
     <div
       css={`
@@ -133,13 +118,6 @@ export default function GrantDetail() {
         justify-content: center;
       `}
     >
-      <div
-        css={`
-          margin-top: 3rem;
-        `}
-      >
-        <BreadCrumbs />
-      </div>
       <PageHeader
         isDetail
         title={grantInfoData.title}
--- src/app/modules/grants-module/index.tsx
+++ src/app/modules/grants-module/index.tsx
@@ -1,12 +1,10 @@
 /* third-party */
 import React from "react";
 import get from "lodash/get";
-import { useRecoilState } from "recoil";
 import { useLocation } from "react-router-dom";
 import { useCMSData } from "app/hooks/useCMSData";
 import { useMediaQuery } from "@material-ui/core";
 import Pagination from "@material-ui/lab/Pagination";
-import { breadCrumbItems } from "app/state/recoil/atoms";
 import { useStoreActions, useStoreState } from "app/state/store/hooks";
 import {
   useTitle,
@@ -18,7 +16,6 @@ import {
 import { PageHeader } from "app/components/PageHeader";
 import { ToolBoxPanel } from "app/components/ToolBoxPanel";
 import { PageLoader } from "app/modules/common/page-loader";
-import BreadCrumbs from "app/components/Charts/common/breadcrumbs";
 import { PageTopSpacer } from "app/modules/common/page-top-spacer";
 import { useDatasetMenuItems } from "app/hooks/useDatasetMenuItems";
 import { GrantListItemModel } from "app/modules/grants-module/data";
@@ -39,7 +36,6 @@ interface GrantsModuleProps {
 export default function GrantsModule(props: GrantsModuleProps) {
   const location = useLocation();
   const cmsData = useCMSData({ returnData: true });
-  const [_, setBreadCrumList] = useRecoilState(breadCrumbItems);
 
   useTitle(
     `${get(cmsData, "modulesGrants.titleStart", "")}${
@@ -159,10 +155,6 @@ export default function GrantsModule(props: GrantsModuleProps) {
     return 0;
   }
 
-  React.useEffect(() => {
-    setBreadCrumList([{ name: "Grants", path: location.pathname, id: "" }]);
-  }, []);
-
   return (
     <div
       css={`
@@ -174,13 +166,6 @@ export default function GrantsModule(props: GrantsModuleProps) {
         justify-content: center;
       `}
     >
-      <div
-        css={`
-          margin-top: 3rem;
-        `}
-      >
-        <BreadCrumbs />
-      </div>
       {(isLoading || loading) && <PageLoader />}
       {!props.code && (
         <>
