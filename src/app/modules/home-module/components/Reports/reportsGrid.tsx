ccebef956da3ecf9bf64733014ec0b106195e645
okorie2 <ellahokorie@gmail.com>
2023-03-21 07:13:19 +0100

    feat: app main page logic implementation

--- src/app/modules/home-module/components/Reports/reportsGrid.tsx
+++ src/app/modules/home-module/components/Reports/reportsGrid.tsx
@@ -28,7 +28,7 @@ export default function ReportsGrid() {
     <Grid container spacing={2}>
       <ReportAddnewCard />
       {loadedDataThemes.map((data) => (
-        <Grid item xs={12} sm={6} md={6} lg={3}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
           <GridItem
             key={data.id}
             id={data.id}
