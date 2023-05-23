82e299b92db9e93eb7bdb31c9f2af44ad0acb559
okorie2 <ellahokorie@gmail.com>
2023-05-23 13:06:22 +0100

    feat: add grid and table icons to  grants dataset card

--- src/app/assets/icons/charts/Grid.tsx
+++ src/app/assets/icons/charts/Grid.tsx
@@ -5,7 +5,7 @@ export const GridIcon = () => {
   return (
     <svg
       width="20"
-      height="18"
+      height="24"
       viewBox="0 0 20 18"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
