8af6c8c47b2d29f6c47d1f6328940e4c02888f80
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2023-06-27 12:07:28 +0300

    chore: add error boundary

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,47 +1,45 @@
 // cc:application base#;application providers
-import React, { ReactNode } from "react";
+import React from "react";
 import theme from "app/theme";
 import { RecoilRoot } from "recoil";
 import { store } from "app/state/store";
-import { AppBar } from "app/components/AppBar";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { BrowserRouter as Router } from "react-router-dom";
 import { PageLoader } from "app/modules/common/page-loader";
 import { StoreProvider, useStoreRehydrated } from "easy-peasy";
+import { ErrorBoundaryDX } from "app/components/ErrorBoundary";
 import { StylesProvider, CssBaseline } from "@material-ui/core";
-import { useLocation } from "react-use";
-import Onboarding from "./modules/onboarding-module";
 
 type ProviderProps = {
   children: any;
 };
 
 function Providers(props: ProviderProps) {
-  const location = useLocation();
   return (
-    <RecoilRoot>
-      <StylesProvider injectFirst>
-        {/* material ui theme provider */}
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <StoreProvider store={store}>
-            <AppContainer>
-              {/* react router */}
-
-              <Router>
-                <div
-                  css={`
-                    min-height: 100vh;
-                  `}
-                >
-                  {props.children}
-                </div>
-              </Router>
-            </AppContainer>
-          </StoreProvider>
-        </ThemeProvider>
-      </StylesProvider>
-    </RecoilRoot>
+    <ErrorBoundaryDX>
+      <RecoilRoot>
+        <StylesProvider injectFirst>
+          {/* material ui theme provider */}
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <StoreProvider store={store}>
+              <AppContainer>
+                {/* react router */}
+                <Router>
+                  <div
+                    css={`
+                      min-height: 100vh;
+                    `}
+                  >
+                    {props.children}
+                  </div>
+                </Router>
+              </AppContainer>
+            </StoreProvider>
+          </ThemeProvider>
+        </StylesProvider>
+      </RecoilRoot>
+    </ErrorBoundaryDX>
   );
 }
 
