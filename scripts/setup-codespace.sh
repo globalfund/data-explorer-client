9c595bc820e6c8c929f6191cbd44e0a50c6e66d5
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2023-07-28 18:02:51 +0300

    chore: update setup-codespace script

--- scripts/setup-codespace.sh
+++ scripts/setup-codespace.sh
@@ -6,10 +6,13 @@ sudo swapon /tmp/swapfile
 sudo swapon --show
 
 # change the node version to the codebase's desired version
+. /usr/local/share/nvm/nvm.sh
 nvm install 16
 nvm use 16
 
 # setup @rawgraphs/rawgraphs-charts submodule
+git submodule init
+git submodule update
 cd rawgraphs-charts
 yarn install
 yarn build
