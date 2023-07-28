dc33fd0413542fd4e79ef6585d20e6382d40f540
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2023-07-28 17:19:54 +0300

    chore: setup-codespace script

---
+++ scripts/setup-codespace.sh
@@ -0,0 +1,24 @@
+# setup swap memory
+sudo fallocate -l 4G /tmp/swapfile
+sudo chmod 600 /tmp/swapfile
+sudo mkswap /tmp/swapfile
+sudo swapon /tmp/swapfile
+sudo swapon --show
+
+# change the node version to the codebase's desired version
+nvm install 16
+nvm use 16
+
+# setup @rawgraphs/rawgraphs-charts submodule
+cd rawgraphs-charts
+yarn install
+yarn build
+yarn link
+cd ..
+
+# install dependencies
+yarn link "@rawgraphs/rawgraphs-charts"
+yarn install
+
+# run the app
+yarn start
