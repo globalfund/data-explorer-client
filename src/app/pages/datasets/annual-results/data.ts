9a85af98324e184aea1d8aa9bf20c9ccb2bbd5cc
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2024-04-15 16:43:27 +0300

    feat: annual results dataset page stats

---
+++ src/app/pages/datasets/annual-results/data.ts
@@ -0,0 +1,26 @@
+export const geographyGroupingOptions = [
+  { value: "Portfolio View", label: "Portfolio View" },
+];
+
+export const componentsGroupingOptions = [
+  { value: "Grouped", label: "Grouped" },
+  { value: "Un-Grouped", label: "Un-Grouped" },
+];
+
+export const stats: {
+  label: string;
+  value: number | string;
+}[] = [
+  {
+    value: "24.5 million",
+    label: "People on antiretroviral therapy for HIV in 2022.",
+  },
+  {
+    value: "6.7 million",
+    label: "People with TB treated in 2022.",
+  },
+  {
+    value: "220 million",
+    label: "Mosquito nets distributed in 2022.",
+  },
+];
--- src/app/pages/datasets/annual-results/index.tsx
+++ src/app/pages/datasets/annual-results/index.tsx
@@ -1,17 +1,36 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
 import { Dropdown } from "app/components/dropdown";
 import { DatasetPage } from "app/pages/datasets/common/page";
+import {
+  stats,
+  geographyGroupingOptions,
+  componentsGroupingOptions,
+} from "app/pages/datasets/annual-results/data";
 
-const geographyGroupingOptions = [
-  { value: "Portfolio View", label: "Portfolio View" },
-];
-
-const componentsGroupingOptions = [
-  { value: "Grouped", label: "Grouped" },
-  { value: "Un-Grouped", label: "Un-Grouped" },
-];
+const StatComp: React.FC<{
+  label: string;
+  value: number | string;
+}> = (props: { label: string; value: number | string }) => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="flex-start"
+      justifyContent="center"
+    >
+      <Typography fontSize="10px">Annual Results</Typography>
+      <Typography variant="h5" fontWeight="700">
+        {props.value}
+      </Typography>
+      <Typography variant="body2" fontWeight="700">
+        {props.label}
+      </Typography>
+    </Box>
+  );
+};
 
 export const AnnualResultsPage: React.FC = () => {
   const [geographyGrouping, setGeographyGrouping] = React.useState(
@@ -65,7 +84,27 @@ export const AnnualResultsPage: React.FC = () => {
       breadcrumbs={[{ label: "Datasets" }, { label: "Annual Results" }]}
       toolbarRightContent={toolbarRightContent}
     >
-      <Box width="100%" marginTop="50px"></Box>
+      <Box width="100%" marginTop="50px">
+        <Box
+          width="100%"
+          display="flex"
+          marginBottom="50px"
+          flexDirection="row"
+          justifyContent="space-between"
+        >
+          {stats.map((stat) => (
+            <StatComp key={stat.label} label={stat.label} value={stat.value} />
+          ))}
+        </Box>
+        <Divider
+          sx={{
+            left: 0,
+            width: "100vw",
+            position: "absolute",
+            borderColor: "#CFD4DA",
+          }}
+        />
+      </Box>
     </DatasetPage>
   );
 };
