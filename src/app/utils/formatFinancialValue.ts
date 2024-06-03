bd93273429de3a7e161e1c83bdd09b4540173c88
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2024-06-03 16:22:02 +0300

    fix: format financial value util function bugfix

--- src/app/utils/formatFinancialValue.ts
+++ src/app/utils/formatFinancialValue.ts
@@ -2,6 +2,7 @@ export function formatFinancialValue(
   value: number | bigint,
   noCurrency?: boolean
 ): string {
+  if (!value) return `${!noCurrency ? "US$ " : ""}0`;
   return `${!noCurrency ? "US$ " : ""}${value.toLocaleString("en-US", {
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
