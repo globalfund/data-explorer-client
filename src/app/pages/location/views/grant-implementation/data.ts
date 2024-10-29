323ba6262209b0aa21bfd7ee467399df78f866ff
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2024-10-29 16:11:51 +0200

    chore: add missing dropdown option

--- src/app/pages/location/views/grant-implementation/data.ts
+++ src/app/pages/location/views/grant-implementation/data.ts
@@ -1,8 +1,6 @@
 export const CHART_1_DROPDOWN_ITEMS = [
-  {
-    value: "Grouped Component",
-    label: "Grouped Component",
-  },
+  { value: "Grouped Component", label: "Grouped Component" },
+  { label: "Un-Grouped Component", value: "Un-Grouped Component" },
 ];
 
 export const CHART_2_DROPDOWN_ITEMS = [
