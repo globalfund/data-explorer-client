4c737f0b2cb007e4c978e9cfa0374461d746bf52
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2025-09-25 18:58:15 +0300

    feat: report builder page report settings

--- src/app/pages/report-builder/builder/components/report-settings/index.tsx
+++ src/app/pages/report-builder/builder/components/report-settings/index.tsx
@@ -3,28 +3,15 @@ import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
-import Typography from "@mui/material/Typography";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import DownloadIcon from "app/assets/vectors/Download.svg?react";
 import { DraggablePopper } from "app/components/draggable-popper";
 import DuplicateIcon from "app/assets/vectors/Duplicate.svg?react";
+import DeleteBackspace from "app/assets/vectors/DeleteBackspace.svg?react";
 
 export const ReportBuilderPageReportSettings: React.FC<{
   clicked: boolean;
   anchorEl: null | HTMLElement;
   setClicked: React.Dispatch<React.SetStateAction<boolean>>;
 }> = ({ anchorEl, clicked, setClicked }) => {
-  const [pageLayout, setPageLayout] = React.useState("A4");
-  const [orientation, setOrientation] = React.useState("Portrait");
-
-  const handleChangePageLayout = (event: SelectChangeEvent) => {
-    setPageLayout(event.target.value);
-  };
-
-  const handleChangeOrientation = (event: SelectChangeEvent) => {
-    setOrientation(event.target.value);
-  };
-
   return (
     <DraggablePopper
       id="report-builder-report-settings"
@@ -36,7 +23,6 @@ export const ReportBuilderPageReportSettings: React.FC<{
     >
       <Box
         sx={{
-          gap: "10px",
           width: "100%",
           display: "flex",
           flexDirection: "column",
@@ -46,9 +32,6 @@ export const ReportBuilderPageReportSettings: React.FC<{
           },
         }}
       >
-        <Typography fontSize="14px" fontWeight="700">
-          Layout and Style
-        </Typography>
         <Box
           sx={{
             gap: "8px",
@@ -63,50 +46,7 @@ export const ReportBuilderPageReportSettings: React.FC<{
               "> label": {
                 color: "#525252",
               },
-            },
-          }}
-        >
-          <Box>
-            <InputLabel id="page-layout-label">Page Layout</InputLabel>
-            <Select
-              variant="filled"
-              value={pageLayout}
-              labelId="page-layout-label"
-              onChange={handleChangePageLayout}
-              sx={{
-                width: "100%",
-                ".MuiSelect-select": {
-                  paddingTop: "11px",
-                  paddingBottom: "11px",
-                },
-              }}
-            >
-              <MenuItem value="A4">A4</MenuItem>
-              <MenuItem value="A3">A3</MenuItem>
-            </Select>
-          </Box>
-          <Box>
-            <InputLabel id="orientation-label">Orientation</InputLabel>
-            <Select
-              variant="filled"
-              value={orientation}
-              labelId="orientation-label"
-              onChange={handleChangeOrientation}
-              sx={{
-                width: "100%",
-                ".MuiSelect-select": {
-                  paddingTop: "11px",
-                  paddingBottom: "11px",
-                },
-              }}
-            >
-              <MenuItem value="Portrait">Portrait</MenuItem>
-              <MenuItem value="Landscape">Landscape</MenuItem>
-            </Select>
-          </Box>
-          <Box
-            sx={{
-              "> input": {
+              "> input[type='color']": {
                 margin: 0,
                 width: "100%",
                 height: "40px",
@@ -115,62 +55,66 @@ export const ReportBuilderPageReportSettings: React.FC<{
                 borderRadius: "4px",
                 border: "1px solid #868e96",
               },
-            }}
-          >
-            <InputLabel id="background-color-label">
-              Background Color
-            </InputLabel>
-            <input type="color" />
+              "> input[type='number']": {
+                width: "100%",
+                height: "40px",
+                fontSize: "14px",
+                borderStyle: "none",
+                borderRadius: "4px",
+                padding: "0px 16px",
+                background: "#f1f3f5",
+                appearance: "none",
+                MozAppearance: "none",
+                WebkitAppearance: "none",
+                borderBottom: "1px solid #868e96",
+                "::-webkit-inner-spin-button": {
+                  margin: 0,
+                  appearance: "none",
+                  WebkitAppearance: "none",
+                },
+                "::-webkit-outer-spin-button": {
+                  margin: 0,
+                  appearance: "none",
+                  WebkitAppearance: "none",
+                },
+              },
+            },
+          }}
+        >
+          <Box>
+            <InputLabel id="width-label">Width</InputLabel>
+            <input type="number" min={200} defaultValue={1074} />
           </Box>
           <Box>
-            <InputLabel id="spacing-label">Spacing</InputLabel>
-            <Box
-              sx={{
-                gap: "5px",
-                display: "flex",
-                flexDirection: "row",
-                "> input": {
-                  width: "100%",
-                  height: "40px",
-                  color: "#98a1aa",
-                  borderStyle: "none",
-                  borderRadius: "4px",
-                  textAlign: "center",
-                  background: "#f1f3f5",
-                  appearance: "none",
-                  MozAppearance: "none",
-                  WebkitAppearance: "none",
-                  borderBottom: "1px solid #868e96",
-                  "::-webkit-inner-spin-button": {
-                    margin: 0,
-                    appearance: "none",
-                    WebkitAppearance: "none",
-                  },
-                  "::-webkit-outer-spin-button": {
-                    margin: 0,
-                    appearance: "none",
-                    WebkitAppearance: "none",
-                  },
-                },
-              }}
-            >
-              <input type="number" min={0} defaultValue={40} />
-              <input type="number" min={0} defaultValue={40} />
-              <input type="number" min={0} defaultValue={40} />
-              <input type="number" min={0} defaultValue={40} />
-            </Box>
+            <InputLabel id="height-label">Height</InputLabel>
+            <input type="number" min={200} defaultValue={1520} />
+          </Box>
+          <Box>
+            <InputLabel id="padding-label">Padding</InputLabel>
+            <input type="number" min={0} max={50} defaultValue={0} />
+          </Box>
+          <Box>
+            <InputLabel id="margin-label">Margin</InputLabel>
+            <input type="number" min={0} max={50} defaultValue={0} />
+          </Box>
+          <Box>
+            <InputLabel id="stroke-label">Stroke</InputLabel>
+            <input type="number" min={0} max={50} defaultValue={0} />
+          </Box>
+          <Box>
+            <InputLabel id="stroke-color-label">Stroke Color</InputLabel>
+            <input type="color" />
           </Box>
         </Box>
-        <Divider />
-        <Typography fontSize="14px" fontWeight="700">
-          File
-        </Typography>
-        <MenuItem>
-          <DownloadIcon /> Download Report
-        </MenuItem>
+        <Divider sx={{ mt: "8px", mb: "4px" }} />
         <MenuItem>
           <DuplicateIcon />
-          Duplicate Report
+          Duplicate report
+        </MenuItem>
+        <Divider sx={{ m: "4px 0 !important" }} />
+        <MenuItem>
+          <DeleteBackspace />
+          Delete report
         </MenuItem>
       </Box>
     </DraggablePopper>
