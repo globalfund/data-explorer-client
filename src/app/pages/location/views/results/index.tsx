ea67f61f3a8196d0c478dd42b45c1df25ebabeb1
Stefanos Hadjipetrou <stefanos@zimmerman.team>
2025-11-11 12:03:12 +0200

    chore: incremental

--- src/app/pages/location/views/results/index.tsx
+++ src/app/pages/location/views/results/index.tsx
@@ -118,6 +118,7 @@ export const Results: React.FC<ResultsProps> = (props: ResultsProps) => {
   const showResults = dataResultsTable.length > 0 || tableSearch.length > 0;
   const showDocuments =
     dataDocumentsTable.length > 0 || tableSearch2.length > 0;
+
   return (
     <Box gap="24px" display="flex" flexDirection="column">
       {showStats && <HomeResultsStats stats={dataResultStats} />}
